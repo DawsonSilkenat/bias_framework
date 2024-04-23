8ff673ed948e2b99126f402b5c7f74d05022078c
Dawson <dawsonsilkenat@gmail.com>
2024-04-23 12:31:56 +0100

    Updated again to make imports easy

--- bias_framework.py
+++ bias_framework.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 from ml_dummy import ML_Model
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, matthews_corrcoef
+from metrics import *
+import plotly.graph_objs as go
+
+
 from aif360.datasets import StandardDataset
-from aif360.metrics import ClassificationMetric
+from aif360.algorithms.preprocessing import Reweighing
 
 
 
@@ -31,6 +34,9 @@ class Bias_Framework:
         
               
         self.privilege_function = lambda x: False
+        
+        self.__fairea = None
+        self.__metrics_by_debiasing_technique = dict()
     
     
     def set_privilege_function(self, function: callable) -> None:
@@ -50,6 +56,8 @@ class Bias_Framework:
             
             Example from aif360 documentation:
             [{'sex': 1, 'age': 1}, {'sex': 0}]
+            The first dictionary indicates that if sex has value 1 and age has value 1 then the individual belongs to the privileged group
+            The second dictionary indicates that if sex has value 0 then the individual belongs to the privileged group (regardless of age)
         """
         self.privilege_function = lambda x: int(
             # Checks if a row matches any of the valid groupings
@@ -59,87 +67,122 @@ class Bias_Framework:
         )
     
     
-    def set_unprivileged_combinations(self, privileged_combinations: list[dict[str: any]]) -> None:
-        """Update the function which determines if an element belongs to the privileged or unprivileged class by specifying which groupings are considered privileged
+    def set_unprivileged_combinations(self, unprivileged_combinations: list[dict[str: any]]) -> None:
+        """Update the function which determines if an element belongs to the privileged or unprivileged class by specifying which groupings are not considered privileged
 
         Args:
-            privileged_combinations: a list of dictionaries, each dictionary maps a set of columns name to the values they must take. Note: Each columns name must map to a single value, not a list of possible values. The values of any column names not included in the dictionary are ignored
+            unprivileged_combinations: a list of dictionaries, each dictionary maps a set of columns name to the values they must take. Note: Each columns name must map to a single value, not a list of possible values. The values of any column names not included in the dictionary are ignored
             
             Example from aif360 documentation:
             [{'sex': 1, 'age': 1}, {'sex': 0}]
+            The first dictionary indicates that if sex has value 1 and age has value 1 then the individual belongs to the unprivileged group
+            The second dictionary indicates that if sex has value 0 then the individual belongs to the unprivileged group (regardless of age)
         """
         self.privilege_function = lambda x: int(
             # Checks if a row matches any of the valid groupings, and negates it so we are unprivileged
             not any([
                 # Checks if all the requirements for a particular grouping are met
-                all([x[name] == value for name, value in grouping]) for grouping in privileged_combinations])
+                all([x[name] == value for name, value in grouping]) for grouping in unprivileged_combinations])
         )
-        
-    
-    def get_error_metrics(self, y_true_value: np.array, y_predicted_value: np.array) -> dict[str, float]:
-        """Computes a number of error metrics returns a dictionary containing the error metrics names and values.
-
-        Args:
-            y_true_value (np.array): The true classes for a dataset
-            y_predicted_value (np.array): The model predicted classes for the same dataset as y_true_value
-
-        Returns:
-            dict[str, float]: A dictionary of metric name to metric value
-        """
-    
-        # These metrics were selected due to their use in the Bias Mitigation Methods paper. There exist more we could include, but not a priority at this stage
-        metrics = dict()
-        metrics["accuracy"]                         = accuracy_score(y_true_value, y_predicted_value)
-        metrics["recall positive class"]            = recall_score(y_true_value, y_predicted_value, pos_label=1)
-        metrics["recall negative class"]            = recall_score(y_true_value, y_predicted_value, pos_label=0)
-        metrics["recall macro average"]             = recall_score(y_true_value, y_predicted_value, average="macro")
-        metrics["precision positive class"]         = precision_score(y_true_value, y_predicted_value, pos_label=1)
-        metrics["precision negative class"]         = precision_score(y_true_value, y_predicted_value, pos_label=0)
-        metrics["precision macro average"]          = precision_score(y_true_value, y_predicted_value, average="macro")
-        metrics["f1 score positive class"]          = f1_score(y_true_value, y_predicted_value, pos_label=1)
-        metrics["f1 score negative class"]          = f1_score(y_true_value, y_predicted_value, pos_label=0)
-        metrics["f1 score macro average"]           = f1_score(y_true_value, y_predicted_value, average="macro")
-        metrics["Matthews correlation coefficient"] = matthews_corrcoef(y_true_value, y_predicted_value)
-    
-        return metrics
     
     
-    def get_fairness_metrics(df: pd.DataFrame, true_values: np.array, predicted_values: np.array, privilege_function: callable) -> dict[str, float]:
-        
-        df["Is Privileged"] = df.apply(privilege_function, axis=1)
-        
-        df_dataset_with_true_class = df.copy()
-        df_dataset_with_true_class["Class Label"] = true_values
-        
-        df_dataset_with_predicted_class = df.copy()
-        df_dataset_with_predicted_class["Class Label"] = predicted_values
-        
-        # I don't believe the values of protected_attribute_names and privileged_classes matter here since this is really set in ClassificationMetric, however these are required fields so might as well set them reasonably. 
-        dataset_with_true_class = StandardDataset(df_dataset_with_true_class, 
-                          label_name="Class Label", 
-                          favorable_classes=[1],
-                          protected_attribute_names=["Is Privileged"], 
-                          privileged_classes=[1]
-                          )
-        
-        dataset_with_predicted_class = StandardDataset(df_dataset_with_true_class, 
-                          label_name="Class Label", 
-                          favorable_classes=[1],
-                          protected_attribute_names=["Is Privileged"], 
-                          privileged_classes=[1]
-                          )
-        
+    def run_framework(self):
+        fairea_args = self.__no_debiasing()
+        self.__fairea = fairea_model_mutation(*fairea_args)
+        self.__reweighing()
     
-        class_metric = ClassificationMetric(dataset_with_true_class,dataset_with_predicted_class, unprivileged_groups=[{"Is Privileged" : 0}], privileged_groups=[{"Is Privileged" : 1}])
     
-        metrics = dict()
-        metrics["statistical parity difference"] = abs(class_metric.statistical_parity_difference())
-        metrics["average odds difference"]       = abs(class_metric.average_abs_odds_difference())
-        metrics["equal opportunity difference"]  = abs(class_metric.equal_opportunity_difference())
-        metrics["error_rate_difference"]         = abs(class_metric.error_rate_difference())
-        
-        return metrics
-
-
-    def fairea_model_mutation(self, df: pd.DataFrame, true_values: np.array, predicted_values: np.array, privilege_function: callable, mutation_percentages: list[float] = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]):
+    def show_fairea_graph(self, debias_methodology: str, error_metric: str, fairness_metric: str) -> None:
+        figure = self.__create_plot(debias_methodology, error_metric, fairness_metric)
+        figure.update_layout(title=f"")
+    
+    
+    def show_many_fairea_graphs(self, debias_methodology: str, error_metric: list[str], fairness_metric: list[str]) -> None:
+        if isinstance(error_metric, str):
+            error_metric = [error_metric]
+        if isinstance(fairness_metric, str):
+            fairness_metric = [fairness_metric]
+        # TODO
         pass
+    
+    
+    def show_all_fairea_graphs(self) -> None:
+        self 
+        
+    
+    def __create_plot(self, debias_methodology: str, error_metric: str, fairness_metric: str) -> go.Figure:
+        fairea_labels = []
+        fairea_x = []
+        fairea_y = []
+        
+        for mutation, metric in self.__fairea:
+            fairea_labels.append(mutation)
+            fairea_x.append(metric["fairness"][fairness_metric])
+            fairea_y.append(metric["error"][error_metric])
+            
+
+        fairea_curve = go.Scatter(x=fairea_x, y=fairea_y, mode="lines", name="fairea baseline", text=fairea_labels)
+        
+        debias_x = self.__metrics_by_debiasing_technique[debias_methodology]["fairness"][fairness_metric]
+        debias_y = self.__metrics_by_debiasing_technique[debias_methodology]["error"][error_metric]
+        debias_result = go.Scatter(x=[debias_x], y=[debias_y], mode="markers", name="debiasing outcome")
+        
+        layout = go.Layout(xaxis_title=f"Bias ({fairness_metric})", yaxis_title=f"Accuracy ({error_metric})")
+        return go.Figure(data=[debias_result, fairea_curve], layout=layout)
+    
+    
+    def get_debias_methodologies(self) -> list[str]:
+        return self.__metrics_by_debiasing_technique.keys()
+    
+    
+    def get_error_metric_names(self) -> list[str]:
+        some_debias = self.__metrics_by_debiasing_technique.keys()[0]
+        return self.__metrics_by_debiasing_technique[some_debias]["error"].keys()
+            
+                 
+    def get_bias_metric_names(self) -> list[str]:
+        some_debias = self.__metrics_by_debiasing_technique.keys()[0]
+        return self.__metrics_by_debiasing_technique[some_debias]["fairness"].keys()
+    
+    
+    def get_raw_data(self) -> list[str]:
+        return self.__metrics_by_debiasing_technique             
+    
+    
+    def __no_debiasing(self):
+        self.model.fit(self.df_x_train, self.df_y_train)
+        predicted_values = self.model.predict(self.df_x_validate)
+        true_values = self.df_y_validate.to_numpy()
+        
+        self.__metrics_by_debiasing_technique["no debiasing"] = get_all_metrics(self.df_x_validate, true_values, predicted_values, self.privilege_function)
+        
+        # Most of these functions won't return anything, this one does so we can run fairea without recomputing
+        return self.df_x_validate, true_values, predicted_values, self.privilege_function
+        
+        
+    def __reweighing(self):
+        # Creating a StandardDataset with the required information about privileged group
+        df_train = self.df_x_train.copy()
+        df_train["Is Privileged"] = df_train.apply(self.privilege_function, axis=1)
+        df_train["Class Label"] = self.df_y_train.to_numpy()
+        
+        training_dataset = StandardDataset(df_train, 
+                    label_name="Class Label", 
+                    favorable_classes=[1],
+                    protected_attribute_names=["Is Privileged"], 
+                    privileged_classes=[[1]]
+                    )
+        
+        # Applying reweighing to the training data
+        reweighing = Reweighing(unprivileged_groups=[{"Is Privileged" : 0}], privileged_groups=[{"Is Privileged" : 1}])
+        transformed_data = reweighing.fit_transform(training_dataset)
+        self.model.fit(transformed_data.features, transformed_data.labels.ravel())
+        
+        # Applying the required modifications to the validation data and getting results for metric calculation
+        df_x_validate = self.df_x_validate.copy()
+        df_x_validate["Is Privileged"] = df_x_validate.apply(self.privilege_function, axis=1)
+        predicted_values = self.model.predict(df_x_validate)
+        true_values = self.df_y_validate.to_numpy()
+        
+        self.__metrics_by_debiasing_technique["reweighing"] = get_all_metrics(self.df_x_validate, true_values, predicted_values, self.privilege_function)
+
---
+++ metrics.py
@@ -0,0 +1,109 @@
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, matthews_corrcoef
+from aif360.datasets import StandardDataset
+from aif360.metrics import ClassificationMetric
+import numpy as np
+import pandas as pd
+
+def get_error_metrics(y_true_value: np.array, y_predicted_value: np.array) -> dict[str, float]:
+    """Computes a number of error metrics returns a dictionary containing the error metrics names and values.
+    Args:
+        y_true_value (np.array): The true classes for a dataset
+        y_predicted_value (np.array): The model predicted classes for the same dataset as y_true_value
+    Returns:
+        dict[str, float]: A dictionary of metric name to metric value
+    """
+
+    # These metrics were selected due to their use in the Bias Mitigation Methods paper. There exist more we could include, but not a priority at this stage
+    metrics = dict()
+    metrics["accuracy"]                         = accuracy_score(y_true_value, y_predicted_value)
+    metrics["recall positive class"]            = recall_score(y_true_value, y_predicted_value, pos_label=1)
+    metrics["recall negative class"]            = recall_score(y_true_value, y_predicted_value, pos_label=0)
+    metrics["recall macro average"]             = recall_score(y_true_value, y_predicted_value, average="macro")
+    metrics["precision positive class"]         = precision_score(y_true_value, y_predicted_value, pos_label=1)
+    metrics["precision negative class"]         = precision_score(y_true_value, y_predicted_value, pos_label=0)
+    metrics["precision macro average"]          = precision_score(y_true_value, y_predicted_value, average="macro")
+    metrics["f1 score positive class"]          = f1_score(y_true_value, y_predicted_value, pos_label=1)
+    metrics["f1 score negative class"]          = f1_score(y_true_value, y_predicted_value, pos_label=0)
+    metrics["f1 score macro average"]           = f1_score(y_true_value, y_predicted_value, average="macro")
+    metrics["Matthews correlation coefficient"] = matthews_corrcoef(y_true_value, y_predicted_value)
+
+    return metrics
+
+
+def get_fairness_metrics(df_validation: pd.DataFrame, true_values: np.array, predicted_values: np.array, privilege_function: callable) -> dict[str, float]:
+    
+    df_validation = df_validation.copy()
+    df_validation["Is Privileged"] = df_validation.apply(privilege_function, axis=1)
+    
+    df_dataset_with_true_class = df_validation.copy()
+    df_dataset_with_true_class["Class Label"] = true_values
+    
+    df_dataset_with_predicted_class = df_validation.copy()
+    df_dataset_with_predicted_class["Class Label"] = predicted_values
+    
+    # I don't believe the values of protected_attribute_names and privileged_classes matter here since this is really set in ClassificationMetric, however these are required fields so might as well set them reasonably. 
+    dataset_with_true_class = StandardDataset(df_dataset_with_true_class, 
+                      label_name="Class Label", 
+                      favorable_classes=[1],
+                      protected_attribute_names=["Is Privileged"], 
+                      privileged_classes=[[1]]
+                      )
+    
+    dataset_with_predicted_class = StandardDataset(df_dataset_with_true_class, 
+                      label_name="Class Label", 
+                      favorable_classes=[1],
+                      protected_attribute_names=["Is Privileged"], 
+                      privileged_classes=[[1]]
+                      )
+    
+
+    class_metric = ClassificationMetric(dataset_with_true_class, dataset_with_predicted_class, unprivileged_groups=[{"Is Privileged" : 0}], privileged_groups=[{"Is Privileged" : 1}])
+
+    metrics = dict()
+    metrics["statistical parity difference"] = abs(class_metric.statistical_parity_difference())
+    metrics["average odds difference"]       = abs(class_metric.average_abs_odds_difference())
+    metrics["equal opportunity difference"]  = abs(class_metric.equal_opportunity_difference())
+    metrics["error rate difference"]         = abs(class_metric.error_rate_difference())
+    
+    return metrics
+
+
+def get_all_metrics(df_validation: pd.DataFrame, true_values: np.array, predicted_values: np.array, privilege_function: callable) -> dict[str, dict[str, float]]:
+    return {
+        "fairness" : get_fairness_metrics(df_validation, true_values, predicted_values, privilege_function),
+        "error" : get_error_metrics(true_values, predicted_values)
+    }
+    
+    
+def fairea_model_mutation(df_validation: pd.DataFrame, true_values: np.array, predicted_values: np.array, privilege_function: callable, fractions_to_mutate: list[float] = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1], repetitions=50) -> list[tuple[float, dict[str, dict[str, float]]]]:
+    # Source: https://solar.cs.ucl.ac.uk/pdf/hort2021fairea.pdf
+    
+    # The Fairea paper used the mutation strategy where all values were mutated to the same label, and the label to mutate to was the one which produced the highest accuracy with 100% mutation. We follow this suggestion.
+    mutate_to_value = np.argmax(np.bincount(true_values))
+    
+    metrics = []
+    
+    for mutation_fraction in fractions_to_mutate:
+        number_to_mutate = int(mutation_fraction * len(predicted_values))
+        
+        # List of metric results from each repetition of the mutation, to be averaged at the end
+        mutation_fraction_metrics = []
+        
+        for _ in range(repetitions):
+            # Creating the mutated predictions and retrieving metrics
+            indexes_to_mutate = np.random.choice(len(predicted_values), number_to_mutate, replace=False)
+            mutated_predictions = np.copy(predicted_values)
+            mutated_predictions[indexes_to_mutate] = mutate_to_value
+    
+            mutation_fraction_metrics.append(get_all_metrics(df_validation, true_values, mutated_predictions, privilege_function))
+        # Average the results across the iterations and append to the results
+        mutation_fraction_metrics_averages = dict()
+        for metric_type, metric_type_dict in mutation_fraction_metrics[0].items():
+            mutation_fraction_metrics_averages[metric_type] = dict()
+            
+            for metric_name in metric_type_dict.keys():
+                mutation_fraction_metrics_averages[metric_type][metric_name] = np.mean([metric[metric_type][metric_name] for metric in mutation_fraction_metrics])
+        
+        metrics.append((mutation_fraction, mutation_fraction_metrics_averages))
+        
+    return metrics
